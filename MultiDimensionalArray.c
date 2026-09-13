ffcbde02997ee1ecba0698d13db00edee5f97b82
Nidhi Dudhankar <nidhi.rjnd@gmail.com>
2026-09-13 18:48:13 +0530

    Add MultiDimensionalArray.c for 2D array input/output

---
+++ MultiDimensionalArray.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+int a[2][2] ;
+int i,j;
+for (i=0;i<2;i++)
+    for (j=0;j<2;j++){
+        printf("enter a[%d][%d]=\t",i,j);
+        scanf("%d", &a[i][j]);
+    }
+for (i=0;i<2;i++)
+    for (j=0;j<2;j++)
+printf("[%d] [%d] = %d\n", i, j, a[i][j]);
+
+return 0;
+}
